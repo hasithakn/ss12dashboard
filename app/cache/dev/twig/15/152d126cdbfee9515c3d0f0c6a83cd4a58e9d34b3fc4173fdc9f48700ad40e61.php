dbe5f22f33739957501988f0417cbb93aded3642
hasithakn <hasithakn@hotmail.com>
2017-08-03 20:53:22 +0530

    5th

--- app/cache/dev/twig/15/152d126cdbfee9515c3d0f0c6a83cd4a58e9d34b3fc4173fdc9f48700ad40e61.php
+++ app/cache/dev/twig/15/152d126cdbfee9515c3d0f0c6a83cd4a58e9d34b3fc4173fdc9f48700ad40e61.php
@@ -22,83 +22,83 @@ class __TwigTemplate_15234f2e87390b365c1db8ff596b5568794c7092c269f9fb6444c6e7524
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_37ed8d8a4225253c9cab92b9ba85fcff3a71a47118d2b422e09f861f672c8edc = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
-        $__internal_37ed8d8a4225253c9cab92b9ba85fcff3a71a47118d2b422e09f861f672c8edc->enter($__internal_37ed8d8a4225253c9cab92b9ba85fcff3a71a47118d2b422e09f861f672c8edc_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "user/profile.html.twig"));
+        $__internal_fb7efc47650088679b24a1004d971779100c0745db00066ccf5e1e55fdde4463 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_fb7efc47650088679b24a1004d971779100c0745db00066ccf5e1e55fdde4463->enter($__internal_fb7efc47650088679b24a1004d971779100c0745db00066ccf5e1e55fdde4463_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "user/profile.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_37ed8d8a4225253c9cab92b9ba85fcff3a71a47118d2b422e09f861f672c8edc->leave($__internal_37ed8d8a4225253c9cab92b9ba85fcff3a71a47118d2b422e09f861f672c8edc_prof);
+        $__internal_fb7efc47650088679b24a1004d971779100c0745db00066ccf5e1e55fdde4463->leave($__internal_fb7efc47650088679b24a1004d971779100c0745db00066ccf5e1e55fdde4463_prof);
 
     }
 
     // line 3
     public function block_body($context, array $blocks = array())
     {
-        $__internal_6c1a5afbda2253f1610a6c8750242d7a689497e60f6617fbe41cbff815214129 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
-        $__internal_6c1a5afbda2253f1610a6c8750242d7a689497e60f6617fbe41cbff815214129->enter($__internal_6c1a5afbda2253f1610a6c8750242d7a689497e60f6617fbe41cbff815214129_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_2f14c81e41dfa806c8376403058c2db63f372d8905ae33ee0f9f819e9da3081e = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_2f14c81e41dfa806c8376403058c2db63f372d8905ae33ee0f9f819e9da3081e->enter($__internal_2f14c81e41dfa806c8376403058c2db63f372d8905ae33ee0f9f819e9da3081e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
 
         // line 4
         echo "
     ";
         // line 5
         $this->displayBlock('javascripts', $context, $blocks);
-        // line 35
+        // line 57
         echo "
     <h2>Hello ";
-        // line 36
+        // line 58
         echo twig_escape_filter($this->env, (isset($context["email"]) ? $context["email"] : $this->getContext($context, "email")), "html", null, true);
         echo "</h2>
     ";
-        // line 37
+        // line 59
         $context['_parent'] = $context;
         $context['_seq'] = twig_ensure_traversable((isset($context["compartments"]) ? $context["compartments"] : $this->getContext($context, "compartments")));
         foreach ($context['_seq'] as $context["_key"] => $context["compartment"]) {
-            // line 38
+            // line 60
             echo "        <div style=\"background-color: #0088CC\">
             <h4 id=\"";
-            // line 39
+            // line 61
             echo twig_escape_filter($this->env, $this->getAttribute($context["compartment"], "compartmentId", array()), "html", null, true);
             echo "compartmentId\"> ";
             echo twig_escape_filter($this->env, $this->getAttribute($context["compartment"], "compartmentId", array()), "html", null, true);
             echo "</h4>
             <h4 id=\"";
-            // line 40
+            // line 62
             echo twig_escape_filter($this->env, $this->getAttribute($context["compartment"], "compartmentId", array()), "html", null, true);
             echo "item\"> ";
             echo twig_escape_filter($this->env, $this->getAttribute($context["compartment"], "item", array()), "html", null, true);
             echo "</h4>
             <h5 id=\"";
-            // line 41
+            // line 63
             echo twig_escape_filter($this->env, $this->getAttribute($context["compartment"], "compartmentId", array()), "html", null, true);
             echo "edate\"> ";
             echo twig_escape_filter($this->env, twig_slice($this->env, $this->getAttribute($this->getAttribute($context["compartment"], "expireDate", array()), "date", array()), 0, 10), "html", null, true);
             echo "</h5>
             <h5 id=\"";
-            // line 42
+            // line 64
             echo twig_escape_filter($this->env, $this->getAttribute($context["compartment"], "compartmentId", array()), "html", null, true);
             echo "sdate\"> ";
             echo twig_escape_filter($this->env, twig_slice($this->env, $this->getAttribute($this->getAttribute($context["compartment"], "storeDate", array()), "date", array()), 0, 10), "html", null, true);
             echo "</h5>
             <h5 id=\"";
-            // line 43
+            // line 65
             echo twig_escape_filter($this->env, $this->getAttribute($context["compartment"], "compartmentId", array()), "html", null, true);
             echo "temp\"> ";
             echo twig_escape_filter($this->env, $this->getAttribute($context["compartment"], "temp", array()), "html", null, true);
             echo "</h5>
             <h5 id=\"";
-            // line 44
+            // line 66
             echo twig_escape_filter($this->env, $this->getAttribute($context["compartment"], "compartmentId", array()), "html", null, true);
             echo "humid\"> ";
             echo twig_escape_filter($this->env, $this->getAttribute($context["compartment"], "humid", array()), "html", null, true);
             echo "</h5>
             <h5 id=\"";
-            // line 45
+            // line 67
             echo twig_escape_filter($this->env, $this->getAttribute($context["compartment"], "compartmentId", array()), "html", null, true);
             echo "foodStatus\"> ";
             echo twig_escape_filter($this->env, $this->getAttribute($context["compartment"], "food_status", array()), "html", null, true);
             echo "</h5>
             <h5 id=\"";
-            // line 46
+            // line 68
             echo twig_escape_filter($this->env, $this->getAttribute($context["compartment"], "compartmentId", array()), "html", null, true);
             echo "timestamp\"> ";
             echo twig_escape_filter($this->env, $this->getAttribute($context["compartment"], "time_stamp", array()), "html", null, true);
@@ -109,26 +109,27 @@ class __TwigTemplate_15234f2e87390b365c1db8ff596b5568794c7092c269f9fb6444c6e7524
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['compartment'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 49
+        // line 71
         echo "
 ";
         
-        $__internal_6c1a5afbda2253f1610a6c8750242d7a689497e60f6617fbe41cbff815214129->leave($__internal_6c1a5afbda2253f1610a6c8750242d7a689497e60f6617fbe41cbff815214129_prof);
+        $__internal_2f14c81e41dfa806c8376403058c2db63f372d8905ae33ee0f9f819e9da3081e->leave($__internal_2f14c81e41dfa806c8376403058c2db63f372d8905ae33ee0f9f819e9da3081e_prof);
 
     }
 
     // line 5
     public function block_javascripts($context, array $blocks = array())
     {
-        $__internal_8c505ea1b01c1a1576cf5f500d00881e9361d2694cc889a82acee61574b62369 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
-        $__internal_8c505ea1b01c1a1576cf5f500d00881e9361d2694cc889a82acee61574b62369->enter($__internal_8c505ea1b01c1a1576cf5f500d00881e9361d2694cc889a82acee61574b62369_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
+        $__internal_f8bbaf5c3320b98b203568bf1405f759e44ebac08847d8fa2bc29b342842fc52 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_f8bbaf5c3320b98b203568bf1405f759e44ebac08847d8fa2bc29b342842fc52->enter($__internal_f8bbaf5c3320b98b203568bf1405f759e44ebac08847d8fa2bc29b342842fc52_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
 
         // line 6
         echo "        <script>
             \$(document).ready(function () {
                 console.log('ready');
+                ajaxReq(\"ESp-01\");
                 var compartments = ";
-        // line 9
+        // line 10
         echo twig_jsonencode_filter((isset($context["compartments"]) ? $context["compartments"] : $this->getContext($context, "compartments")));
         echo ";
                 jQuery.each(compartments, function (i, compartment) {
@@ -140,24 +141,45 @@ class __TwigTemplate_15234f2e87390b365c1db8ff596b5568794c7092c269f9fb6444c6e7524
 
 
                 function ajaxReq(compartmentId) {
-                    \$.get(\"http://localhost/ss12dashboard/web/app_dev.php/api2\",
-                            {
-                                'compartmentId': compartmentId
-                            },
-                            function (data) {
-                                console.log(data[0]);
-                                \$(\"#\" + compartmentId + \"temp\").html(\"temp: \" + data[0].temp);
-                                \$(\"#\" + compartmentId + \"humid\").html(\"humid: \" + data[0].humid);
-                                \$(\"#\" + compartmentId + \"foodStatus\").html(\"foodStatus: \" + data[0].food_status);
-                                \$(\"#\" + compartmentId + \"timestamp\").html(\"Time: \" + data[0].time_stamp);
-                            });
+//                    \$.get(\"http://localhost/ss12dashboard/web/app_dev.php/api2\",
+//                            {
+//                                'compartmentId': compartmentId
+//                            },
+//                            function (data) {
+//                                console.log(data[0]);
+//                                \$(\"#\" + compartmentId + \"temp\").html(\"temp: \" + data[0].temp);
+//                                \$(\"#\" + compartmentId + \"humid\").html(\"humid: \" + data[0].humid);
+//                                \$(\"#\" + compartmentId + \"foodStatus\").html(\"foodStatus: \" + data[0].food_status);
+//                                \$(\"#\" + compartmentId + \"timestamp\").html(\"Time: \" + data[0].time_stamp);
+//                            });
+                    \$.ajax({
+                        type: \"GET\",
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'text/plain'
+                        },
+                        dataType: \"json\",
+                        url: \"http://45.55.91.141/ss12dashboard/web/app_dev.php/api2\",
+                        data: {'compartmentId': compartmentId},
+                        success: function (data) {
+                            console.log(data[0]);
+                            \$(\"#\" + compartmentId + \"temp\").html(\"temp: \" + data[0].temp);
+                            \$(\"#\" + compartmentId + \"humid\").html(\"humid: \" + data[0].humid);
+                            \$(\"#\" + compartmentId + \"foodStatus\").html(\"foodStatus: \" + data[0].food_status);
+                            \$(\"#\" + compartmentId + \"timestamp\").html(\"Time: \" + data[0].time_stamp);
+                        },
+                        error: function (xhr, ajaxOptions, thrownError) {
+                            console.log(xhr.status);
+                            console.log(thrownError);
+                        }
+                    });
                 }
 
             });
         </script>
     ";
         
-        $__internal_8c505ea1b01c1a1576cf5f500d00881e9361d2694cc889a82acee61574b62369->leave($__internal_8c505ea1b01c1a1576cf5f500d00881e9361d2694cc889a82acee61574b62369_prof);
+        $__internal_f8bbaf5c3320b98b203568bf1405f759e44ebac08847d8fa2bc29b342842fc52->leave($__internal_f8bbaf5c3320b98b203568bf1405f759e44ebac08847d8fa2bc29b342842fc52_prof);
 
     }
 
@@ -173,7 +195,7 @@ class __TwigTemplate_15234f2e87390b365c1db8ff596b5568794c7092c269f9fb6444c6e7524
 
     public function getDebugInfo()
     {
-        return array (  132 => 9,  127 => 6,  121 => 5,  113 => 49,  102 => 46,  96 => 45,  90 => 44,  84 => 43,  78 => 42,  72 => 41,  66 => 40,  60 => 39,  57 => 38,  53 => 37,  49 => 36,  46 => 35,  44 => 5,  41 => 4,  35 => 3,  11 => 1,);
+        return array (  133 => 10,  127 => 6,  121 => 5,  113 => 71,  102 => 68,  96 => 67,  90 => 66,  84 => 65,  78 => 64,  72 => 63,  66 => 62,  60 => 61,  57 => 60,  53 => 59,  49 => 58,  46 => 57,  44 => 5,  41 => 4,  35 => 3,  11 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -194,6 +216,7 @@ class __TwigTemplate_15234f2e87390b365c1db8ff596b5568794c7092c269f9fb6444c6e7524
         <script>
             \$(document).ready(function () {
                 console.log('ready');
+                ajaxReq(\"ESp-01\");
                 var compartments = {{ compartments|json_encode|raw }};
                 jQuery.each(compartments, function (i, compartment) {
                     console.log(compartment.compartment_id);
@@ -204,17 +227,38 @@ class __TwigTemplate_15234f2e87390b365c1db8ff596b5568794c7092c269f9fb6444c6e7524
 
 
                 function ajaxReq(compartmentId) {
-                    \$.get(\"http://localhost/ss12dashboard/web/app_dev.php/api2\",
-                            {
-                                'compartmentId': compartmentId
-                            },
-                            function (data) {
-                                console.log(data[0]);
-                                \$(\"#\" + compartmentId + \"temp\").html(\"temp: \" + data[0].temp);
-                                \$(\"#\" + compartmentId + \"humid\").html(\"humid: \" + data[0].humid);
-                                \$(\"#\" + compartmentId + \"foodStatus\").html(\"foodStatus: \" + data[0].food_status);
-                                \$(\"#\" + compartmentId + \"timestamp\").html(\"Time: \" + data[0].time_stamp);
-                            });
+//                    \$.get(\"http://localhost/ss12dashboard/web/app_dev.php/api2\",
+//                            {
+//                                'compartmentId': compartmentId
+//                            },
+//                            function (data) {
+//                                console.log(data[0]);
+//                                \$(\"#\" + compartmentId + \"temp\").html(\"temp: \" + data[0].temp);
+//                                \$(\"#\" + compartmentId + \"humid\").html(\"humid: \" + data[0].humid);
+//                                \$(\"#\" + compartmentId + \"foodStatus\").html(\"foodStatus: \" + data[0].food_status);
+//                                \$(\"#\" + compartmentId + \"timestamp\").html(\"Time: \" + data[0].time_stamp);
+//                            });
+                    \$.ajax({
+                        type: \"GET\",
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'text/plain'
+                        },
+                        dataType: \"json\",
+                        url: \"http://45.55.91.141/ss12dashboard/web/app_dev.php/api2\",
+                        data: {'compartmentId': compartmentId},
+                        success: function (data) {
+                            console.log(data[0]);
+                            \$(\"#\" + compartmentId + \"temp\").html(\"temp: \" + data[0].temp);
+                            \$(\"#\" + compartmentId + \"humid\").html(\"humid: \" + data[0].humid);
+                            \$(\"#\" + compartmentId + \"foodStatus\").html(\"foodStatus: \" + data[0].food_status);
+                            \$(\"#\" + compartmentId + \"timestamp\").html(\"Time: \" + data[0].time_stamp);
+                        },
+                        error: function (xhr, ajaxOptions, thrownError) {
+                            console.log(xhr.status);
+                            console.log(thrownError);
+                        }
+                    });
                 }
 
             });
